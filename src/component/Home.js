2e0209c3338063ffe0329c1e4ba554785b9b51eb
Omolade Sunday <sundayomoladee11@gmail.com>
2023-02-16 03:04:20 -0800

    home completed

--- src/component/Home.js
+++ src/component/Home.js
@@ -37,14 +37,16 @@ const Home = () => {
           </div>
         </div>
       </article>
-      {/* <article className="descovery">
-        <div className="right">
-          <img src={image1} alt="try" />
-        </div>
+      <article className="descovery">
         <div className="left">
-          <h2>Discover</h2>
-          <h3>Immerse Yourself In The Beautiful and Evolving Virtual Word</h3>
-          <h5>
+          <div className="homeImageContainer">
+            <img src={image1} alt="try" />
+          </div>
+        </div>
+        <div className="right">
+          <h2 className="discoverText">Trade</h2>
+          <h3>Buy & sell LANDs $ Estate From Our Store</h3>
+          <h5 className="immerseText">
             Discover LANDs owned by users & experience incredible scenes and
             structures
           </h5>
@@ -54,7 +56,16 @@ const Home = () => {
             </Link>
           </div>
         </div>
-      </article> */}
+      </article>
+      <section className="homeAboutContainer">
+        <h2 className="discoverText">About MintyLand</h2>
+        <h3 className='homeAboutText'>MintyLand allow you to buy and sell virtual assets and real estate</h3>
+        <div className="getStarted">
+          <Link className="btn " to="/about">
+            Get Started
+          </Link>
+        </div>
+      </section>
     </div>
   );
 }
