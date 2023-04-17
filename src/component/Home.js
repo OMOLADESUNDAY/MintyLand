2a3eb8fccd6fb2ae8d9d004ea087d8dc320f1ba2
omolade sunday <sundayomoladee11@gmail.com>
2023-04-17 11:41:11 +0100

    editing homepage

--- src/component/Home.js
+++ src/component/Home.js
@@ -7,26 +7,38 @@ const Home = () => {
   let image1=Image1
   return (
     <div className="container">
+   
       <div className=" homeContainer">
         <Navbar />
         <div className="wrapper">
+       
           <div className="cover">
+                  
             <h2 className="welcomeText">Welcome To</h2>
-            <h1 className="mintyText">MintyLand</h1>
+              
+              <div style={{display:"flex", width:"fit-content",height:"80px" , alignItems:"flex-end"}}>
+               
+                <h1 className="mintyText colorful-text">MintyLand </h1>
+              <div class="box-container" style={{margin:"-2rem"}}>
+                <div class="box box1"></div>
+                <div class="box box2"></div>
+            </div>  
+              </div>
+               
             <h2 className="buyText">Buy, sell and own virtual lands</h2>
           </div>
         </div>
       </div>
       <article className="descovery">
-        <div className="left">
-          <h2 className="discoverText">Discover</h2>
+        <div className="left" >
+          <h2 className="discoverText colorful-text">Discover</h2>
           <h3>Immerse Yourself In The Beautiful and Evolving Virtual Word</h3>
           <h5 className="immerseText">
             Discover LANDs owned by users & experience incredible scenes and
             structures
           </h5>
           <div className="getStarted">
-            <Link className="btn " to="/signup">
+            <Link className="btn getSbtn" to="/signup">
               Get Started
             </Link>
           </div>
@@ -44,24 +56,24 @@ const Home = () => {
           </div>
         </div>
         <div className="right">
-          <h2 className="discoverText">Trade</h2>
+          <h2 className="discoverText colorful-text">Trade</h2>
           <h3>Buy & sell LANDs $ Estate From Our Store</h3>
           <h5 className="immerseText">
             Discover LANDs owned by users & experience incredible scenes and
             structures
           </h5>
           <div className="getStarted">
-            <Link className="btn " to="/signup">
+            <Link className="btn getSbtn" to="/signup">
               Get Started
             </Link>
           </div>
         </div>
       </article>
       <section className="homeAboutContainer">
-        <h2 className="discoverText">About MintyLand</h2>
+        <h2 className="discoverText colorful-text">About MintyLand</h2>
         <h3 className='homeAboutText'>MintyLand allow you to buy and sell virtual assets and real estate</h3>
         <div className="getStarted">
-          <Link className="btn " to="/about">
+          <Link className="btn getSbtn" to="/about">
             Read More
           </Link>
         </div>
